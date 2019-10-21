621f3151f4f6a3155d3877cc4e53ab8fca70f7c8
Misaki Koonce <misaki.koonce@gmail.com>
2019-10-20 19:27:01 -0700

    Grab HTML elements.

---
+++ js/main.js
@@ -0,0 +1,7 @@
+(function(){
+    const canvas = document.querySelector('.photo');
+    const ctx = canvas.getContext('2d');
+    const player = document.querySelector('.player');
+    const strip = document.querySelector('.strip');
+    const snap = document.querySelector('.snap');
+}());
