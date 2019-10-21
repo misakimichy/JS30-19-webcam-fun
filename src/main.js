c2a1f67f8474de25cfb2a55920ecd3cec9ee35fa
Misaki Koonce <misaki.koonce@gmail.com>
2019-10-20 21:12:07 -0700

    Add getVideo function.

--- src/main.js
+++ src/main.js
@@ -1,8 +1,17 @@
-// import 'styles.css';
-// (function(){
-//   const canvas = document.querySelector('.photo');
-//   const ctx = canvas.getContext('2d');
-//   const player = document.querySelector('.player');
-//   const strip = document.querySelector('.strip');
-//   const snap = document.querySelector('.snap');
-// }());
+import './styles.css';
+(function(){
+  const canvas = document.querySelector('.photo');
+  const ctx = canvas.getContext('2d');
+  const video = document.querySelector('.player');
+  const strip = document.querySelector('.strip');
+  const snap = document.querySelector('.snap');
+
+  const getVideo = () => {
+    navigator.mediaDevices.getUserMedia({ video: true, audio: false })
+        .then(localMediaStream => {
+            video.src = window.URL.createObjectURL(localMediaStream);
+            video.play();
+        });
+  };
+  getVideo();
+}());
