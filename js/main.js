ff56fd1a8dedccf061bc9c3b9746d58db0ec420b
Misaki Koonce <misaki.koonce@gmail.com>
2019-10-20 20:56:12 -0700

    Install webpack

--- js/main.js
+++
@@ -1,7 +0,0 @@
-(function(){
-    const canvas = document.querySelector('.photo');
-    const ctx = canvas.getContext('2d');
-    const player = document.querySelector('.player');
-    const strip = document.querySelector('.strip');
-    const snap = document.querySelector('.snap');
-}());
---
+++ src/main.js
@@ -0,0 +1,8 @@
+// import 'styles.css';
+// (function(){
+//   const canvas = document.querySelector('.photo');
+//   const ctx = canvas.getContext('2d');
+//   const player = document.querySelector('.player');
+//   const strip = document.querySelector('.strip');
+//   const snap = document.querySelector('.snap');
+// }());
