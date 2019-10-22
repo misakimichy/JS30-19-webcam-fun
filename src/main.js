62bd647949f88988280c7a55b8f8f2138cd57935
Misaki Koonce <misaki.koonce@gmail.com>
2019-10-21 22:36:59 -0700

    Add takePhoto function.

--- src/main.js
+++ src/main.js
@@ -1,33 +1,48 @@
 import './styles.css';
 (function(){
-  const canvas = document.querySelector('.photo');
-  const ctx = canvas.getContext('2d');
-  const video = document.querySelector('.player');
-  const strip = document.querySelector('.strip');
-  const snap = document.querySelector('.snap');
+    const canvas = document.querySelector('.photo');
+    const ctx = canvas.getContext('2d');
+    const video = document.querySelector('.player');
+    const strip = document.querySelector('.strip');
+    const snap = document.querySelector('.snap');
+    let filter = '';
 
-  const getVideo = () => {
-    navigator.mediaDevices.getUserMedia({ video: true, audio: false })
-        .then(MediaStream => {
-            video.srcObject = MediaStream;
-            video.play();
-        }).catch(error => {
-            console.log('You denied the webcam access', error);
+    const getVideo = () => {
+        navigator.mediaDevices.getUserMedia({ video: true, audio: false })
+            .then(MediaStream => {
+                video.srcObject = MediaStream;
+                video.play();
+            }).catch(error => {
+                console.log('You denied the webcam access', error);
         });
-  };
+    };
 
-  const paintCanvas = () => {
-      const width = video.videoWidth;
-      const height = video.videoHeight;
-      canvas.width = width;
-      canvas.height = height;
+    const paintCanvas = () => {
+        const width = video.videoWidth;
+        const height = video.videoHeight;
+        canvas.width = width;
+        canvas.height = height;
 
-      return setInterval(() => {
-          ctx.drawImage(video, 0, 0, width, height);
-          //take the pixels out
-      }, 16);
-  };
+        return setInterval(() => {
+            ctx.drawImage(video, 0, 0, width, height);
+            //take the pixels out
+            let pixels = ctx.getImageData(0, 0, width, height);
+        }, 16);
+    };
 
-  getVideo();
-  paintCanvas();
+    const takePhoto = () => {
+        // play the sound
+        snap.currentTime = 0;
+        snap.play();
+        //take the data out of the canvas
+        const data = canvas.toDataURL('images/jpeg');
+        const link = document.createElement('a');
+        link.href = data;
+        link.setAttribute('download', 'handsome');
+        link.textContent = 'Download Image';
+        strip.insertBefore(link, strip.firstChild);
+        };
+
+    getVideo();
+    video.addEventListener('canplay',paintCanvas);
 }());
